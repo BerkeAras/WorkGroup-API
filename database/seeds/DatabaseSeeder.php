3778a0578789594daa504eec118e66c956bae874
Berke Aras <info@berkearas.de>
2021-11-28 21:03:54 +0100

    App Settings

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(GroupTableSeeder::class);
+        $this->call(AppSettingsTableSeeder::class);
     }
 }
